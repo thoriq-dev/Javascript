df25e4939f311cf1debcef3de93493e4fb546998
thoriq-dev <thoriqtnf07@gmail.com>
2024-01-08 15:16:08 +0700

    Menambahkan file memanggil function.js

---
+++ Function/Function_Memanggil.js
@@ -0,0 +1,10 @@
+function jumlahDuaBilangan(a, b) {
+  var total;
+  total = a + b;
+
+  return total;
+}
+
+alert(jumlahDuaBilangan(1, 2));
+alert(jumlahDuaBilangan(25, 30));
+alert(jumlahDuaBilangan(1500, 17.5));
