6395da02c679ad2eafdbf340b164cc2298e571de
thoriq-dev <thoriqtnf07@gmail.com>
2024-01-19 11:20:54 +0700

    Menyelesaikan 3 prinsip dasar array

---
+++ Menipulasi_Array/menampilkan-array.js
@@ -0,0 +1,5 @@
+let arrayThoriq = ["Thoriq", 21, "Single", "Islam", "Tangerang"];
+
+for (let i = 0; i < arrayThoriq.length; i++) {
+  console.log(arrayThoriq[i]);
+}
---
+++ Menipulasi_Array/menghapus-array.js
@@ -0,0 +1,5 @@
+let arrayThoriq = ["Thoriq", 21, "Single", "Islam", "Tangerang"];
+
+arrayThoriq[1] = undefined;
+
+console.log(arrayThoriq);
