421e248df6aba4aae28bb0f3f4d71b6765c597f3
thoriq-dev <thoriqtnf07@gmail.com>
2024-01-20 02:30:18 +0700

    Menambah & memperbaiki repository penumpang angkot.js

--- Penumpang_Angkot/penumpang-angkot.js
+++ Penumpang_Angkot/penumpang-angkot.js
@@ -1,22 +1,36 @@
-let penumpang = [];
+let penumpang = ["Thoriq", undefined, "Micko"];
 
-function tambahDataPenumpang(nama, status) {
-  let dataPenumpang = {
-    nama: nama,
-    status: status,
-  };
-  penumpang.push(dataPenumpang);
-  console.log(`Penumpang ${nama} naik kedalam angkot`);
-}
-
-function tampilkanDataPenumpang() {
-  console.log("Isi Penumpang dalam Angkot :");
-
-  penumpang.forEach(function (dataPenumpang, index) {
-    console.log(`Kursi ke-[${index + 1}] Nama: ${dataPenumpang.nama}, `);
-  });
-}
-
-tambahDataPenumpang("Thoriq", "Penumpang");
-
-tampilkanDataPenumpang();
+let tambahPenumpang = function (namaPenumpang, penumpang) {
+  // Jika angkot kosong
+  if (penumpang.length == 0) {
+    // tambah penumpang di awal array
+    penumpang.push(namaPenumpang);
+    // kembalikan isi array & keluar dari function
+    return penumpang;
+  } else {
+    // Else
+    // Telusuri seluruh kursi di awal
+    for (var i = 0; i < penumpang.length; i++) {
+      // Jika ada kursi kosong
+      if (penumpang[i] == undefined) {
+        // tambah penumpang di kursi tersebut
+        penumpang[i] = namaPenumpang;
+        // kembalikan isi array & keluar dari function
+        return penumpang;
+      }
+      // Jika sudah ada nama yang sama
+      else if (penumpang[i] == namaPenumpang) {
+        console.log(namaPenumpang + " Sudah ada didalam angkot");
+      }
+      // tampilkan pesan kesalahannya
+      // kembalikan isi array & keluar dari function
+      // Jika seluruh kursi terisi
+      else if (i == penumpang.length - 1) {
+        // tambah penumpang di akhir array
+        penumpang.push(namaPenumpang);
+        // kembalikkan isi array & keluar dari function
+        return penumpang;
+      }
+    }
+  }
+};
